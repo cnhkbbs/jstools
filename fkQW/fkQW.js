396450b0f529f6710892b5dca8ae714f5b9b7774
bui~bui~bui~bui <53457442+cnhkbbs@users.noreply.github.com>
2023-03-21 13:44:23 +0800

    Update fkQW.js

--- fkQW/fkQW.js
+++ fkQW/fkQW.js
@@ -7,7 +7,7 @@
         <div
           style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center; background-color: gold; width: 100%; height: 100%;">
           <div style="border: solid;margin: 12%; padding: 3%;text-align: center;">
-            <p style="color: black;font-size: 65px;">由于特殊原因，请复制当前网址到浏览器打开</p>
+            <p style="color: black;font-size: 65px;">由于我们没有对当前浏览器适配，请复制当前网址到其它浏览器打开</p>
             <div onclick="copyToClipboard()" style="width: 70%;height: 5%; background-color: black; margin: 0 auto;">
               <a style="color: white;font-size: 500%;">复制网址</a>
             </div>
