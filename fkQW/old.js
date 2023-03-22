231233c3c246d50a90b72032d09fb2431a60dbdf
bui~bui~bui~bui <53457442+cnhkbbs@users.noreply.github.com>
2023-03-22 14:29:36 +0800

    Create old.js

---
+++ fkQW/old.js
@@ -0,0 +1,30 @@
+(function () {
+  let ua = navigator.userAgent.toLowerCase();
+  if ((ua.match(/MicroMessenger/i) == 'micromessenger') || (ua.match(/QQ/i) == 'qq')) {
+    const body = document.querySelector('body');
+    if (body) {
+      body.innerHTML = `
+        <div
+          style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center; background-color: gold; width: 100%; height: 100%;">
+          <div style="border: solid;margin: 12%; padding: 3%;text-align: center;">
+            <p style="color: black;font-size: 65px;">由于我们没有对当前浏览器适配，请复制当前网址到其它浏览器打开</p>
+            <div onclick="copyToClipboard()" style="width: 70%;height: 5%; background-color: black; margin: 0 auto;">
+              <a style="color: white;font-size: 500%;">复制网址</a>
+            </div>
+          </div>
+        </div>
+      `;
+      function copyToClipboard(){
+        const el = document.createElement('textarea');
+        el.value = window.location.href;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+        alert('复制成功，请到浏览器地址栏粘贴打开');
+      }
+    } else {
+      console.error('Cannot set properties of null (setting \'innerHTML\')');
+    }
+  }
+})();
